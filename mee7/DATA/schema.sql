382a2cbcc2143b87ad0615085f7b375126ef23fc
SpyrosRoum <spyrosr1@gmail.com>
2020-02-22 00:08:34 +0200

    Update .gitignore & add schema.sql

--- mee7/DATA/schema.sql
+++ mee7/DATA/schema.sql
@@ -1,5 +1,7 @@
 CREATE TABLE public.settings (
 	g_id int8 NOT NULL,
 	prefix varchar NOT NULL DEFAULT '!'::character varying,
+	welcome_chn_id int8 NULL,
+	welcome_msg varchar NULL,
 	CONSTRAINT settings_pk PRIMARY KEY (g_id)
 );
