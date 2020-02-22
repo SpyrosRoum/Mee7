50c4be047c569325eff1ca38c73bf40fc8c2a628
SpyrosRoum <spyrosr1@gmail.com>
2020-02-22 12:46:18 +0200

    Add warn related commands

--- mee7/cogs/moderation.py
+++ mee7/cogs/moderation.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from utils import create_pages, Nembed_warnings
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
@@ -45,13 +46,81 @@ class Moderation(commands.Cog):
         """unmute [member]"""
         muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
         if muted_role is None:
-            await ctx.send("This person is not muted")
+            await ctx.send("This person is not muted. \nIf he is, try mutting them again")
 
         await member.remove_roles(muted_role)
         await ctx.send(f"{member.mention} has been unmuted")
 
+    @commands.command(brief='Give a warning to someone')
+    # TODO permissions
+    async def warn(self, ctx, member: discord.Member):
+        """warn [member]"""
+        warnings = await self.bot.pg_con.fetchval(
+            """
+            INSERT INTO members (g_id, m_id, warnings)
+                 VALUES ($1, $2, $3)
+            ON CONFLICT (g_id, m_id)
+            DO
+                UPDATE
+                   SET warnings = members.warnings + 1
+            RETURNING warnings
+            """, ctx.guild.id, member.id, 1
+        )
 
-    # TODO warn, unwarn, warnings (list), lock, unlock (channels)
+        await ctx.send(f"{member.mention} you have been warned and have {warnings} warnings")
+
+    @commands.command(brief='Remove a warning from someone')
+    # TODO permissions
+    async def unwarn(self, ctx, member: discord.Member):
+        """unwarn [member]"""
+        warnings = await self.bot.pg_con.fetchval(
+            """
+            UPDATE members
+               SET warnings = warnings - 1
+             WHERE g_id = $1
+               AND m_id = $2
+               AND warnings > 0
+            RETURNING warnings
+            """, ctx.guild.id, member.id
+        )
+
+        await ctx.send(f"{member.mention} you now have {warnings} warnings")
+
+    @commands.command(brief='Show the warnings of a member or everyone')
+    async def warnings(self, ctx, member: discord.Member=None):
+        """warnings (member)"""
+        if member is not None:
+            warnings = await self.bot.pg_con.fetchval(
+                """
+                SELECT warnings
+                  FROM members
+                 WHERE g_id = $1
+                   AND m_id = $2
+                """, ctx.guild.id, member.id
+            )
+            warnings = warnings or 0
+            embed = discord.Embed(
+                color=ctx.author.color,
+                timestamp=ctx.message.created_at
+            )
+            embed.set_author(name="Warnings", icon_url=member.avatar_url)
+
+            text = (f"Member: {member.mention}\n"
+                    f"Warnings: {warnings}\n")
+            embed.description = text
+
+            await ctx.send(embed=embed)
+        else:
+            warnings = await self.bot.pg_con.fetch(
+                """
+                SELECT warnings
+                  FROM members
+                 WHERE g_id = $1
+                """
+            )
+            await create_pages(ctx, warnings, Nembed_warnings, "Warnings closed")
+
+    # TODO lock, unlock (channels)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
