1e2a91fbd4b43fe85a438461dda6de4287c3bb4b
Spyros Roum <spyrosr1@gmail.com>
2020-05-20 15:43:02 +0300

    Commands to set the min and max reward of eco commands

--- mee7/cogs/set_up_economy.py
+++ mee7/cogs/set_up_economy.py
@@ -1,18 +1,23 @@
 import asyncio
 
-import discord
+from utils import get_name
+
 from discord.ext import commands
 
 
 class SetUpEco(commands.Cog, name="Set up Economy"):
     def __init__(self, bot):
-        self.bot=bot
+        self.bot = bot
 
+    @commands.group(invoke_without_command=True, brief="Configure economy related commands", name="set")
+    async def set_(self, ctx):
+        """set [command]"""
+        pass
 
-    @commands.command(brief='Set the currency name for the server. The `short` by default is same to the name')
+    @set_.command(brief='Set the currency name for the server. The `short` by default is same to the name')
     @commands.has_permissions(administrator=True)
-    async def set_currency(self, ctx, name, short=None):
-        """currency "[name]" "(short)\""""
+    async def currency(self, ctx, name, short=None):
+        """set currency "[name]" "(short)\""""
         short = short or name
 
         await self.bot.pg_con.execute(
@@ -28,6 +33,7 @@ class SetUpEco(commands.Cog, name="Set up Economy"):
     @commands.has_permissions(administrator=True)
     async def update_currency(self, ctx):
         """update_currency"""
+
         def check(msg):
             return msg.author == ctx.author and msg.channel == ctx.channel
 
@@ -82,6 +88,72 @@ class SetUpEco(commands.Cog, name="Set up Economy"):
             await ctx.send("Done")
         else:
             await ctx.send("You need to first set the currency")
+
+    async def set_min_max(self, g_id, cmd_name, min_, max_):
+        await self.bot.pg_con.execute(
+            """
+            INSERT INTO eco_settings (g_id, cmd_name, min, max) 
+                 VALUES ($1, $2, $3, $4)
+            ON CONFLICT (g_id, cmd_name)
+            DO 
+                UPDATE 
+                   SET (min, max) = (excluded.min, excluded.max)
+            """, g_id, cmd_name, min_, max_
+        )
+
+    @set_.command(brief=("Set the min and max money someone can get from the command `slut`. "
+                         "Min and max can be the same"))
+    async def slut(self, ctx, min_: float, max_: float):
+        """set slut [min] [max]"""
+        if min_ > max_:
+            await ctx.send("The minimum money can't be more than the maximum")
+            return
+
+        await self.set_min_max(ctx.guild.id, "slut", min_, max_)
+
+        cur_name = await get_name(self.bot, ctx.guild.id)
+        await ctx.send(f"`slut` will now reward between {min_} and {max_} {cur_name}")
+
+    @set_.command(brief=("Set the min and max money someone can get from the command `work`. "
+                         "Min and max can be the same"))
+    async def work(self, ctx, min_: float, max_: float):
+        """set work [min] [max]"""
+        if min_ > max_:
+            await ctx.send("The minimum money can't be more than the maximum")
+            return
+
+        await self.set_min_max(ctx.guild.id, "work", min_, max_)
+
+        cur_name = await get_name(self.bot, ctx.guild.id)
+        await ctx.send(f"`work` will now reward between {min_} and {max_} {cur_name}")
+
+    @set_.command(brief=("Set the min and max money someone can get from the command `gamble`. "
+                         "Min and max can be the same"))
+    async def gamble(self, ctx, min_: float, max_: float):
+        """set gamble [min] [max]"""
+        if min_ > max_:
+            await ctx.send("The minimum money can't be more than the maximum")
+            return
+
+        await self.set_min_max(ctx.guild.id, "gamble", min_, max_)
+
+        cur_name = await get_name(self.bot, ctx.guild.id)
+        await ctx.send(f"`gamble` will now reward between {min_} and {max_} {cur_name}")
+
+    @set_.command(brief=("Set the min and max money someone can get from the command `crime`. "
+                         "Min and max can be the same"))
+    async def crime(self, ctx, min_: float, max_: float):
+        """set crime [min] [max]"""
+        if min_ > max_:
+            await ctx.send("The minimum money can't be more than the maximum")
+            return
+
+        await self.set_min_max(ctx.guild.id, "crime", min_, max_)
+
+        cur_name = await get_name(self.bot, ctx.guild.id)
+        await ctx.send(f"`crime` will now reward between {min_} and {max_} {cur_name}")
+
+
 
 
 def setup(bot):
